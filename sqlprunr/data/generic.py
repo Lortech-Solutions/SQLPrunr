d4082c30557c957795a099b3b15a2759b60fbfd8
style77 <stylek777@gmail.com>
2024-07-29 15:53:32 +0200

    chore: create more generic database types in OOP-style

--- sqlprunr/data/generic.py
+++ sqlprunr/data/generic.py
@@ -2,20 +2,36 @@ from dataclasses import dataclass
 import typing
 
 
-@dataclass(frozen=True)
-class Column:
+@dataclass
+class Database:
+    """
+    Database dataclass to represent a database
+
+    :param name: Name of the database
+    :param tables: List of tables in the database
+    """
+    name: str
+    schemas: typing.List["Schema"]
+
+    def __hash__(self):
+        return hash(self.name)
+
+
+@dataclass
+class Schema:
     """
     Column dataclass to represent a column in a table
 
     :param name: Name of the column
     """
     name: str
+    tables: typing.List["Table"]
 
     def __hash__(self):
         return hash(self.name)
 
 
-@dataclass(frozen=True)
+@dataclass
 class Table:
     """
     Table dataclass to represent a table in a database
@@ -24,7 +40,21 @@ class Table:
     :param columns: List of columns in the table
     """
     name: str
-    columns: typing.List[Column]
+    columns: typing.List["Column"]
+
+    def __hash__(self):
+        return hash(self.name)
+
+
+@dataclass(frozen=True)
+class Column:
+    """
+    Column dataclass to represent a column in a table
+
+    :param name: Name of the column
+    """
+    name: str
+    data_type: str
 
     def __hash__(self):
         return hash(self.name)
