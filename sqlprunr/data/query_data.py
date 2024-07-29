3eef6ab2ba897ddf8a083b0d02457ad4f24b080f
style77 <stylek777@gmail.com>
2024-07-29 15:55:24 +0200

    chore: Frequencies dataclass

--- sqlprunr/data/query_data.py
+++ sqlprunr/data/query_data.py
@@ -1,13 +1,20 @@
 from dataclasses import dataclass
-import typing
-
-from sqlprunr.data.dimension import Dimension
 
 
 @dataclass
+class Frequencies:
+    tables: dict
+    columns: dict
+    queries: dict
+
+    def __hash__(self) -> int:
+        return hash(self.tables) + hash(self.columns) + hash(self.queries)
+
+@dataclass
 class QueryData:
-    """
-    Represents a query with its dimensions.
-    """
-    query: str
-    dimensions: typing.List[Dimension]
+    QUERY_TEXT: str
+    START_TIME: str
+    END_TIME: str
+
+    def __hash__(self) -> int:
+        return hash(self.QUERY_TEXT) + hash(self.START_TIME) + hash(self.END_TIME)
